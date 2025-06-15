6f7eed5e0a9e49c16776d44f646dc72ab49f9bbf
ernesto <ernestofamorim@gmail.com>
2025-06-15 06:37:19 -0300

    Migrando para novo projeto Spring Boot

---
+++ src/main/java/com/floriano/login_system_backend_maven/LoginSystemBackendMavenApplication.java
@@ -0,0 +1,13 @@
+package com.floriano.login_system_backend_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSystemBackendMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSystemBackendMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floriano/login_system_backend_maven/LoginSystemBackendMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floriano.login_system_backend_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSystemBackendMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
